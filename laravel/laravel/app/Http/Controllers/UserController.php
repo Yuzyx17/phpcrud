7113b4d0f489813344e47b95cfed25f23b4db633
Yuzyx17 <braitboat@gmail.com>
2023-07-22 23:14:36 +0800

    debug

---
+++ laravel/laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,94 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+use App\Models\User; //add user Model - Data is coming from the database via Model.
+ 
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = user::all();
+        return view ('user\index')->with('users', $users);
+    }
+ 
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    public function create()
+    {
+        return view('user\create');
+    }
+ 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        user::create($input);
+        return redirect('user')->with('flash_message', 'user Addedd!');  
+    }
+ 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = user::find($id);
+        return view('user\show')->with('users', $user);
+    }
+ 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = user::find($id);
+        return view('user\edit')->with('users', $user);
+    }
+ 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = user::find($id);
+        $input = $request->all();
+        $user->update($input);
+        return redirect('user')->with('flash_message', 'User Updated!');  
+    }
+ 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        user::destroy($id);
+        return redirect('user')->with('flash_message', 'User deleted!');  
+    }
+}
--- laravel/laravel/routes/web.php
+++ laravel/laravel/routes/web.php
@@ -2,9 +2,11 @@
  
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController; //add the ControllerNameSpace
+use App\Http\Controllers\UserController; //add the ControllerNameSpace
  
 // Route::get('/', function () {
 //     return view('welcome');
 // });
  
-Route::resource("/student", StudentController::class);
+// Route::resource("/student", StudentController::class);
+Route::resource("/user", UserController::class);
