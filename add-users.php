88ad5d096c9aa3376b8aaa566eac7605f6fcfa85
Yuzyx17 <braitboat@gmail.com>
2023-07-22 22:57:56 +0800

    added laravel

--- add-users.php
+++ add-users.php
@@ -84,10 +84,6 @@ if(isset($_REQUEST['submit']) and $_REQUEST['submit']!=""){
 	<meta name="robots" content="index,follow">
 	<title>PHP CRUD</title>
 
-	
-
-	<link rel="shortcut icon" href="https://demo.learncodeweb.com/favicon.ico">
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
